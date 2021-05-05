a6f859deb24ac89f36e805301f1f50dbc9b1fb4c
Locullus <bula.mickael@gmail.com>
2021-05-06 00:19:21 +0200

    premier commit nouvelle appli. Récupère les cours d'une action au moyen d'une API. Les données sont restructurées du format json vers une liste où chaque élément se compose d'une date, closing, opening, higher, lower , volume.

---
+++ StockAPI.py
@@ -0,0 +1,44 @@
+"""
+To claim your free API key with lifetime access, visit this page :
+    https://www.alphavantage.co/support
+"""
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+# fonction qui récupère les variables d'environnement dans le fichier .env
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+API_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}'
+
+stock = 'IBM'
+# on crée un dictionnaire au format texte avec la réponse à une requête reçue en json
+response = requests.get(API_url.format(stock, API_KEY))
+response = json.loads(response.text)
+
+# on crée des listes à partir des clés et des valeurs du dictionnaire
+key_list = list(response['Time Series (Daily)'].keys())
+value_list = [list(element.values()) for element in list(response['Time Series (Daily)'].values())]
+
+# on recrée la structure du dictionnaire mais sous forme d'une liste de tuples
+data_list = list(zip(key_list, value_list))
+
+# on transforme la liste de tuples en liste de listes
+data_list = [list(element) for element in data_list]
+
+# enfin on fait disparaître la sous-liste de chaque élément pour ne garder que des listes composées de strings
+result = []
+for element in data_list:
+    flat_list = [element[0]]
+    for each_element in element[1]:
+        flat_list.append(each_element)
+    result.append(flat_list)
+
+# on affiche le résultat
+for element in result:
+    print(element)
+
+# url qui renvoit le RSI 14 jours. A compléter par symbol=ACTION (ne pas oublier l'API_KEY)
+# https://www.alphavantage.co/query?function=RSI&symbol={}&interval=daily&time_period=14&series_type=close&apikey={}
